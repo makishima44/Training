178f135de1ba565e5bb19aa585e55f18654f6370
Artur <metallist15431543@gmail.com>
2024-03-26 16:08:41 +0300

    2

--- lesson4.js
+++ lesson4.js
@@ -11,3 +11,16 @@ const products = [
 
 let totalCost = products.reduce((acc, product) => acc + product.price, 0);
 console.log(totalCost);
+
+//2)У вас есть объект, представляющий информацию о пользователях.Ваша задача - создать массив, содержащий имена пользователей старше 25 лет.
+
+//Исходный объект
+
+const users = {
+  1: { name: "Alice", age: 30 },
+  2: { name: "Bob", age: 25 },
+  3: { name: "Charlie", age: 35 },
+};
+const userArray = Object.values(users);
+const usersOver25 = userArray.filter((u) => u.age > 25).map((u) => u.name);
+console.log(usersOver25);
